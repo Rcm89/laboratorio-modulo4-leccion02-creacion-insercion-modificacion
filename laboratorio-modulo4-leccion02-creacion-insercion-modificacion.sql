ef9ec764109d2d11e2bf00c2c5278addf4188360
Rcm89 <rcaste1989@gmail.com>
2024-10-22 21:42:10 +0200

    terminado

--- laboratorio-modulo4-leccion02-creacion-insercion-modificacion.sql
+++ laboratorio-modulo4-leccion02-creacion-insercion-modificacion.sql
@@ -155,10 +155,131 @@ update detalles_envios
 set precio_unitario = precio_unitario * 1.1
 where producto = 'Producto A';
 
--- 4. Actualizar la Dirección de un Cliente. Modifica la dirección del cliente con `id_cliente = 2` a "Nueva Calle B, 123".
+ 4. Actualizar la Dirección de un Cliente. Modifica la dirección del cliente con `id_cliente = 2` a "Nueva Calle B, 123".
 update clientes
 set direccion = 'Nueva Calle B, 123'
 where id_cliente = 2;
+
+5. Cambiar el Proveedor de un Envío. Cambia el proveedor del envío con `id_envio = 4` al proveedor con `id_proveedor = 3`.
+update envios 
+set id_proveedor = 3
+where id_envio = 4;
+
+6. Actualizar la Cantidad de un Producto en un Envío. Modifica la cantidad del producto "Producto C" en el envío con `id_envio = 2` a 5 unidades.
+update detalles_envios
+set cantidad = 5
+where id_envio = 2 and producto = 'Producto C';
+
+7. Actualizar el Total de un Envío. Incrementa el total del envío con `id_envio = 5` en 50 unidades monetarias.
+update envios
+set total = 50
+where id_envio = 5;
+
+8. Modificar el Contacto de un Proveedor. Cambia el contacto del proveedor con `id_proveedor = 2` a "Nuevo Contacto 2".
+update proveedores
+set contacto = 'Nuevo Contacto 2'
+where id_proveedor = 2;
+
+9. Cambiar el País de un Cliente. Actualiza el país del cliente con `id_cliente = 6` a "Portugal".
+update clientes
+set pais='Portugal'
+where id_cliente = 6;
+
+10. Actualizar la Fecha de un Envío. Modifica la fecha del envío con `id_envio = 7` a "2024-08-10".
+update envios
+set fecha_envio='2024-08-10'
+where id_envio = 7;
+
+Ejercicio 4: Eliminación de Datos
+
+1. Añadir una Columna. Añade una columna `fecha_nacimiento` de tipo `DATE` a la tabla `Clientes`.
+alter table clientes
+add column fecha_nacimiento date;
+
+2. Añadir una Columna. Añade una columna `codigo_postal` de tipo `VARCHAR(10)` a la tabla `Proveedores`.
+alter table proveedores 
+add column codigo_postal varchar(100);
+
+ 3. Eliminar una Columna. Elimina la columna `contacto` de la tabla `Proveedores`.
+alter table proveedores
+drop column contacto;
+
+ 4. Eliminar una Columna. Elimina la columna `pais` de la tabla `Regiones`.
+alter table regiones
+drop column pais;
+
+ 5. Modificar el Tipo de Dato de una Columna. Modifica el tipo de dato de la columna `telefono` en la tabla `Clientes` a `VARCHAR(15)`.
+alter table clientes
+alter column telefono type varchar(50);
+
+ 6. Modificar el Tipo de Dato de una Columna. Modifica el tipo de dato de la columna `total` en la tabla `Envíos` a `NUMERIC(12, 2)`.
+alter table envios
+alter column total type numeric(12,2);
+
+ 7. Añadir una Columna. Añade una columna `fecha_contrato` de tipo `DATE` a la tabla `Empleados`.
+alter table empleados 
+add column fecha_contrato date;
+
+ 8. Eliminar una Columna. Elimina la columna `estado` de la tabla `Envíos`.
+alter table envios
+drop column estado;
+
+ 9. Modificar el Nombre de una Columna. Modifica el nombre de la columna `nombre` en la tabla `Empleados` a `nombre_completo`.
+alter table empleados
+rename column nombre to nombre_completo;
+
+Ejercicio 5: Contesta a las siguientes preguntas usando queries
+
+ 1. Listar todos los clientes que viven en España.
+select * 
+from clientes
+where pais = 'España';
+
+ 2. Obtener todos los envíos realizados por un empleado específico.
+select *
+from envios
+where id_empleado = 3;
+
+-- 3. Listar todos los productos incluidos en un envío específico.
+select *
+from detalles_envios
+where id_envio = 4;
+
+-- 4. Encontrar todos los proveedores con un teléfono específico.
+select *
+from proveedores
+where telefono = 987654321;
+
+-- 5. Listar los empleados que tienen un cargo de "Supervisor de Envíos".
+select *
+from empleados
+where cargo = 'Supervisor de Envíos';
+
+-- 6. Obtener todos los envíos que fueron realizados por el cliente con id_cliente = 5.
+select *
+from envios
+where id_cliente = 5;
+
+-- 7. Listar todas las regiones con su nombre y país. (Lo del país no hay que hacerlo)
+select nombre
+from regiones;
+
+-- 8. Mostrar todos los productos cuyo precio unitario sea mayor que 50.
+select *
+from detalles_envios
+where precio_unitario > 50;
+
+-- 9. Obtener todos los envíos realizados el 2024-08-05. 
+select *
+from envios
+where fecha_envio = '2024-08-05';
+
+-- 10. Listar todos los clientes que tienen un número de teléfono que comienza con "6003".
+select *
+from clientes
+where telefono like '6001%';
+
+
 
 
 
